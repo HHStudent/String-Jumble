3a0aa5ef48be3dba9d9fc6d14ff6398c1ffca505
HHStudent <dimitrisomoff@hanovernorwichschools.org>
2015-10-28 13:53:58 -0400

    Updated from Brython Server: 10/28/2015 1:53:57 PM

--- stringjumble.py
+++ stringjumble.py
@@ -1,7 +1,7 @@
 """
 stringjumble.py
-Author: <your name>
-Credit: <sources>
+Author: Dimitri
+Credit: http://stackoverflow.com/questions/931092/reverse-a-string-in-python, http://stackoverflow.com/questions/12794141/reverse-input-in-python
 
 Assignment:
 
@@ -25,3 +25,12 @@ ydnah dnif yam uoy taht skcirt ro seuqinhcet wef a era erehT
 handy find may you that tricks or techniques few a are There
 erehT era a wef seuqinhcet ro skcirt taht uoy yam dnif ydnah
 """
+#s = str(input("Please enter a string of text (the bigger the better): "))
+s = str("There are a few techniques or tricks that you may find handy")
+
+print(s[::-1])
+
+s1 = (s[::-1])
+print(' '.join(reversed(s.split(' '))))
+
+print(' '.join(reversed(s1.split(' '))))
